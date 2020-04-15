28652047adb10244c05dd9eeb305d638f7ed39ca
Six4 <tm.timcronin@gmail.com>
2020-04-15 12:02:43 -0400

    cleaned up look for covid page

--- config/ormCovid.js
+++ config/ormCovid.js
@@ -5,16 +5,24 @@ let covidConn = "https://covid19.mathdro.id/api";
 // Object for all our SQL statement functions.
 let orm = {
   all: function(cb) {
-    Promise.all([axios.get(covidConn),axios.get(covidConn + "/daily")])
-    
-    .then((res) => {
+    axios.get(covidConn).then((res) => {
       cb({
-        confirmed: res[1].data[res[1].data.length-1].confirmed.total,
-        recovered: res[0].data.recovered.value,
-        deaths: res[1].data[res[1].data.length-1].deaths.total,
-        lastUpdate: res[1].data[res[1].data.length-1].reportDate
+        confirmed: res.data.confirmed.value,
+        recovered: res.data.recovered.value,
+        deaths: res.data.deaths.value,
+        lastUpdate: res.data.lastUpdate
       });
     });
+    // Promise.all([axios.get(covidConn),axios.get(covidConn + "/daily")])
+    
+    // .then((res) => {
+    //   cb({
+    //     confirmed: res[1].data[res[1].data.length-1].confirmed.total,
+    //     recovered: res[0].data.recovered.value,
+    //     deaths: res[1].data[res[1].data.length-1].deaths.total,
+    //     lastUpdate: res[1].data[res[1].data.length-1].reportDate
+    //   });
+    // });
   },
   country: function(country, cb) {
     axios.get(covidConn+"/countries/"+country).then((res) => {
